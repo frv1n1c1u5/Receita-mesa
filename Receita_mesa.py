6fcbb3fb01772f30ed57206544ec1c16ea113e82
frv1n1c1u5 <vinicius.farias.rocha@gmail.com>
2024-09-18 15:04:35 -0300

    Update Receita_mesa.py

--- Receita_mesa.py
+++ Receita_mesa.py
@@ -4,7 +4,6 @@ import numpy as np
 from datetime import datetime
 import io
 import base64
-import dask.dataframe as dd
 
 # Configuração da página
 st.set_page_config(
@@ -31,14 +30,12 @@ df_corretagem = None
 # Leitura da planilha de Produtos Estruturados
 if uploaded_file_produtos is not None:
     try:
-        df_produtos_pd = pd.read_excel(uploaded_file_produtos)
+        df_produtos = pd.read_excel(uploaded_file_produtos)
         expected_columns_produtos = ['Código Cliente', 'Data da Operação', 'Ação da Estrutura', 'Comissão Gerada', 'Assessor', 'Status']
-        missing_columns = [col for col in expected_columns_produtos if col not in df_produtos_pd.columns]
+        missing_columns = [col for col in expected_columns_produtos if col not in df_produtos.columns]
         if missing_columns:
             st.error("Colunas Faltando na Planilha de Produtos Estruturados: " + ', '.join(missing_columns))
         else:
-            # Converter para Dask dataframe
-            df_produtos = dd.from_pandas(df_produtos_pd, npartitions=1)
             # Filtrar 'Status' == 'Totalmente Executado'
             df_produtos = df_produtos[df_produtos['Status'] == 'Totalmente Executado']
     except Exception as e:
@@ -47,14 +44,12 @@ if uploaded_file_produtos is not None:
 # Leitura da planilha de Corretagem
 if uploaded_file_corretagem is not None:
     try:
-        df_corretagem_pd = pd.read_excel(uploaded_file_corretagem)
+        df_corretagem = pd.read_excel(uploaded_file_corretagem)
         expected_columns_corretagem = ['Código Cliente', 'Data da Operação', 'Comissão BMF', 'Comissão BOV', 'Receita Total', 'Código Assessor', 'Canal']
-        missing_columns = [col for col in expected_columns_corretagem if col not in df_corretagem_pd.columns]
+        missing_columns = [col for col in expected_columns_corretagem if col not in df_corretagem.columns]
         if missing_columns:
             st.error("Colunas Faltando na Planilha de Corretagem: " + ', '.join(missing_columns))
         else:
-            # Converter para Dask dataframe
-            df_corretagem = dd.from_pandas(df_corretagem_pd, npartitions=1)
             # Realizar cálculos
             df_corretagem['Corretagem Bruta'] = df_corretagem['Receita Total']
             df_corretagem['Corretagem Líquida'] = df_corretagem['Corretagem Bruta'] * 0.95
@@ -83,31 +78,29 @@ if (df_produtos is not None) or (df_corretagem is not None):
             st.header("Produtos Estruturados")
 
             # Filtros
-            unique_assessores_prod = df_produtos['Assessor'].drop_duplicates().compute().sort_values()
+            unique_assessores_prod = df_produtos['Assessor'].drop_duplicates().sort_values()
             selected_assessor_prod = st.multiselect("Selecionar Assessor", unique_assessores_prod)
 
-            unique_clientes_prod = df_produtos['Código Cliente'].drop_duplicates().compute().sort_values()
+            unique_clientes_prod = df_produtos['Código Cliente'].drop_duplicates().sort_values()
             selected_cliente_prod = st.multiselect("Selecionar Cliente", unique_clientes_prod)
 
             # Aplicar filtros
+            df_filtered_produtos = df_produtos.copy()
             if selected_assessor_prod:
-                df_produtos = df_produtos[df_produtos['Assessor'].isin(selected_assessor_prod)]
+                df_filtered_produtos = df_filtered_produtos[df_filtered_produtos['Assessor'].isin(selected_assessor_prod)]
             if selected_cliente_prod:
-                df_produtos = df_produtos[df_produtos['Código Cliente'].isin(selected_cliente_prod)]
-
-            # Computar dataframe para pandas
-            df_produtos_display = df_produtos.compute()
+                df_filtered_produtos = df_filtered_produtos[df_filtered_produtos['Código Cliente'].isin(selected_cliente_prod)]
 
             # Formatação de datas e valores monetários
-            df_produtos_display['Data da Operação'] = pd.to_datetime(df_produtos_display['Data da Operação']).dt.strftime('%d/%m/%Y')
-            df_produtos_display['Comissão Gerada'] = df_produtos_display['Comissão Gerada'].apply(lambda x: f"R$ {x:,.2f}")
+            df_filtered_produtos['Data da Operação'] = pd.to_datetime(df_filtered_produtos['Data da Operação']).dt.strftime('%d/%m/%Y')
+            df_filtered_produtos['Comissão Gerada'] = df_filtered_produtos['Comissão Gerada'].apply(lambda x: f"R$ {x:,.2f}")
 
             # Somatório para o card
-            total_comissao_produtos = df_produtos['Comissão Gerada'].sum().compute()
+            total_comissao_produtos = df_filtered_produtos['Comissão Gerada'].replace('[R\$\s,]', '', regex=True).astype(float).sum()
             st.metric(label="Total Comissão Gerada", value=f"R$ {total_comissao_produtos:,.2f}")
 
             # Paginação
-            df_produtos_paginated = paginate_dataframe(df_produtos_display, PAGE_SIZE)
+            df_produtos_paginated = paginate_dataframe(df_filtered_produtos, PAGE_SIZE)
 
             # Exibir tabela
             st.dataframe(df_produtos_paginated)
@@ -116,10 +109,10 @@ if (df_produtos is not None) or (df_corretagem is not None):
             export_option = st.selectbox("Exportar Dados", ["Nenhum", "Excel", "PDF"], key='export_produtos')
             if export_option == "Excel":
                 towrite = io.BytesIO()
-                df_produtos_display.to_excel(towrite, index=False)
+                df_filtered_produtos.to_excel(towrite, index=False)
                 towrite.seek(0)
                 b64 = base64.b64encode(towrite.read()).decode()
-                linko= f'<a href="data:application/octet-stream;base64,{b64}" download="produtos_filtrados.xlsx">Baixar Excel</a>'
+                linko = f'<a href="data:application/octet-stream;base64,{b64}" download="produtos_filtrados.xlsx">Baixar Excel</a>'
                 st.markdown(linko, unsafe_allow_html=True)
             elif export_option == "PDF":
                 st.warning("Exportação para PDF não implementada")
@@ -130,38 +123,36 @@ if (df_produtos is not None) or (df_corretagem is not None):
             st.header("Corretagem")
 
             # Filtros
-            unique_assessores = df_corretagem['Código Assessor'].drop_duplicates().compute().sort_values()
+            unique_assessores = df_corretagem['Código Assessor'].drop_duplicates().sort_values()
             selected_assessor = st.multiselect("Selecionar Assessor", unique_assessores)
 
-            unique_clientes = df_corretagem['Código Cliente'].drop_duplicates().compute().sort_values()
+            unique_clientes = df_corretagem['Código Cliente'].drop_duplicates().sort_values()
             selected_cliente = st.multiselect("Selecionar Cliente", unique_clientes)
 
-            unique_canais = df_corretagem['Canal'].drop_duplicates().compute().sort_values()
+            unique_canais = df_corretagem['Canal'].drop_duplicates().sort_values()
             selected_canal = st.multiselect("Selecionar Canal", unique_canais)
 
             # Aplicar filtros
+            df_filtered_corretagem = df_corretagem.copy()
             if selected_assessor:
-                df_corretagem = df_corretagem[df_corretagem['Código Assessor'].isin(selected_assessor)]
+                df_filtered_corretagem = df_filtered_corretagem[df_filtered_corretagem['Código Assessor'].isin(selected_assessor)]
             if selected_cliente:
-                df_corretagem = df_corretagem[df_corretagem['Código Cliente'].isin(selected_cliente)]
+                df_filtered_corretagem = df_filtered_corretagem[df_filtered_corretagem['Código Cliente'].isin(selected_cliente)]
             if selected_canal:
-                df_corretagem = df_corretagem[df_corretagem['Canal'].isin(selected_canal)]
-
-            # Computar dataframe para pandas
-            df_corretagem_display = df_corretagem.compute()
+                df_filtered_corretagem = df_filtered_corretagem[df_filtered_corretagem['Canal'].isin(selected_canal)]
 
             # Formatação de datas e valores monetários
-            df_corretagem_display['Data da Operação'] = pd.to_datetime(df_corretagem_display['Data da Operação']).dt.strftime('%d/%m/%Y')
+            df_filtered_corretagem['Data da Operação'] = pd.to_datetime(df_filtered_corretagem['Data da Operação']).dt.strftime('%d/%m/%Y')
             currency_columns = ['Comissão BMF', 'Comissão BOV', 'Receita Total', 'Corretagem Bruta', 'Corretagem Líquida', 'Receita Escritório', 'Receita da Mesa']
             for col in currency_columns:
-                df_corretagem_display[col] = df_corretagem_display[col].apply(lambda x: f"R$ {x:,.2f}")
+                df_filtered_corretagem[col] = df_filtered_corretagem[col].apply(lambda x: f"R$ {x:,.2f}")
 
             # Somatório para o card
-            total_receita_mesa = df_corretagem['Receita da Mesa'].sum().compute()
+            total_receita_mesa = df_filtered_corretagem['Receita da Mesa'].replace('[R\$\s,]', '', regex=True).astype(float).sum()
             st.metric(label="Total Receita da Mesa", value=f"R$ {total_receita_mesa:,.2f}")
 
             # Paginação
-            df_corretagem_paginated = paginate_dataframe(df_corretagem_display, PAGE_SIZE)
+            df_corretagem_paginated = paginate_dataframe(df_filtered_corretagem, PAGE_SIZE)
 
             # Exibir tabela
             st.dataframe(df_corretagem_paginated)
@@ -170,10 +161,10 @@ if (df_produtos is not None) or (df_corretagem is not None):
             export_option_corretagem = st.selectbox("Exportar Dados", ["Nenhum", "Excel", "PDF"], key='export_corretagem')
             if export_option_corretagem == "Excel":
                 towrite = io.BytesIO()
-                df_corretagem_display.to_excel(towrite, index=False)
+                df_filtered_corretagem.to_excel(towrite, index=False)
                 towrite.seek(0)
                 b64 = base64.b64encode(towrite.read()).decode()
-                linko= f'<a href="data:application/octet-stream;base64,{b64}" download="corretagem_filtrada.xlsx">Baixar Excel</a>'
+                linko = f'<a href="data:application/octet-stream;base64,{b64}" download="corretagem_filtrada.xlsx">Baixar Excel</a>'
                 st.markdown(linko, unsafe_allow_html=True)
             elif export_option_corretagem == "PDF":
                 st.warning("Exportação para PDF não implementada")
